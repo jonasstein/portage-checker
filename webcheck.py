d4f159931237c01cddae9c3e96b889ddcd569836
Jonas Stein <news@jonasstein.de>
2013-03-29 16:57:36 +0100

    write output to .html file

--- webcheck.py
+++ webcheck.py
@@ -7,20 +7,20 @@ import portage
 #import urllib
 import mechanize
 
-def printhead():
-    print("""
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+htmlstring_FAIL = """<basefont color="#FF2020">FAIL</font>"""
+htmlstring_OK = """<basefont color="#20FF20">OK</font>"""
+
+htmlstring_HEAD = """ <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de">
 <head>
 <title>gentoo ebuild checker</title>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 </head>
-""")
+"""
 
+htmlstring_FOOTER = """</table> </body> </html>"""
 
-htmlstring_FAIL = """<basefont color="#FF2020">FAIL</font>"""
-htmlstring_OK = """<basefont color="#20FF20">OK</font>"""
 
 def browsertest(url):
     br = mechanize.Browser()
@@ -31,15 +31,17 @@ def browsertest(url):
     except:
         return(htmlstring_FAIL)
     
-
+def pout(mystring):
+    f = open("result.html", "a")
+    f.write(mystring)
+    f.close()
 
 def main():
     # init
     porttree = portage.db[portage.root]['porttree']
 
-
-    printhead()
-    print("""<body><table>""")
+    pout(htmlstring_HEAD)
+    pout("""<body><table>""")
 
     all_cps = porttree.dbapi.cp_all()[1:30]
 
@@ -53,13 +55,14 @@ def main():
                        'version':cp,
                        'homepage':porttree.dbapi.aux_get(bestcpv, ["HOMEPAGE"])[0],
                        'webstatus':browsertest(porttree.dbapi.aux_get(bestcpv, ["HOMEPAGE"])[0])}
+            print(cp,bestcpv)
       
-            print("""<tr><td>%(name)s</td>
+            pout("""<tr><td>%(name)s</td>
             <td><a href=%(homepage)s> %(homepage)s </a></td>
             <td>%(webstatus)s</td></tr> """ % package)
 
 
-    print("""</table> </body> </html>""")
+    pout(htmlstring_FOOTER)
 
 
 
